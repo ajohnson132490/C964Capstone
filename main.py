695fa2f8669b007e01ef16b48a6fbed67a16704d
Austin Johnson <ajohnson132490@gmail.com>
2025-01-31 13:57:36 -0600

    cleaning up from cali data

--- main.py
+++ main.py
@@ -3,9 +3,7 @@ from flask import Flask, request, render_template
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
-from sklearn.datasets import fetch_california_housing
 
  
 # Flask constructor
@@ -25,6 +23,7 @@ def index():
     return render_template("index.html")
 
 def xAxisToInt(xAxis):
+    # Hard code the list to map translation of the towns and types
     townDict = {'Branford': 1, 'Trumbull': 2, 'Madison': 3, 'East Granby': 4, 'Simsbury': 5, 'Bridgewater': 6, 'Beacon Falls': 7, 'Sterling': 8,
                  'Groton': 9, 'Waterford': 10, 'Glastonbury': 11, 'Canton': 12, 'Woodstock': 13, 'Putnam': 14, 'Shelton': 15, 'Ansonia': 16,
                    'Plymouth': 17, 'Avon': 18, 'Canaan': 19, 'Morris': 20, 'Willington': 21, 'North Haven': 22, 'Columbia': 23, 'Thomaston': 24,
@@ -50,10 +49,12 @@ def xAxisToInt(xAxis):
                                                            'New Haven': 148, 'East Haddam': 149, 'Bristol': 150, 'Montville': 151, 'Barkhamsted': 152,
                                                              'Wethersfield': 153, 'Windsor': 154}
     typeDict = {'Single Family': 1, 'Two Family' : 2, 'Three Family' : 3, 'Four Family' : 4, 'Condo' : 5, np.nan: 6}
+    
+    # Create lists for the output
     townList = []
     typeList = []
 
-
+    # Adding the key for every item into a list sequentially
     for item in xAxis['Town']:
         townList.append(townDict[item])
     
@@ -64,9 +65,7 @@ def xAxisToInt(xAxis):
 
 if __name__=='__main__':
     #app.run()
-    # Get the data from sklearn and create the model
-    california_housing = fetch_california_housing()
-
+    
     # Create the model
     model = LinearRegression()
 
@@ -76,29 +75,19 @@ if __name__=='__main__':
     typeData = []
     saleData = []
 
-    # Assign the data (features) to the X and Z axis
-    #X = pd.DataFrame(california_housing.data, columns=california_housing.feature_names)
+    # Assign the data to the X and Z axis and formatting it for training
     xAxis = pd.DataFrame({'Town': rawData["Town"], 'Type': rawData["Residential Type"]})
-    # Assign the target (home price) to the Y axis
-    #y = pd.Series(california_housing.target)
-    yAxis = pd.Series(rawData["Sale Amount"])
-
-    # Adding the features for the visual
-
-    ### DECIDE WHICH TWO THINGS TO USE, MAYBE AVG ROOMS AND HOUSE AGE???
-    # https://inria.github.io/scikit-learn-mooc/python_scripts/datasets_california_housing.html
-    #X = X[['MedInc', 'AveBedrms']]
-
-    # 100% of the data needs to go into training
-    #X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=1, random_state=1)
-
-    # Actually train the model
-    #model.fit(X_train, y_train)
-    
     xAxis = xAxisToInt(xAxis)
     xTrain = pd.DataFrame({'Town': list(xAxis['Town'].keys()), 'Type': list(xAxis['Type'].keys())})
+    
+    # Assign the target (home price) to the Y axis
+    yAxis = pd.Series(rawData["Sale Amount"])
+
+    # Training the model
     model.fit(xTrain, yAxis)
-    # Make a prediction?
+
+    # TODO: Get the data from the html form and use it as an input for newData
+    # Make a prediction based on the user inputted town and residence type
     newData = pd.DataFrame({'Town': [60,60,60,60], 'Type': [1,2,3,5]})
     y_pred = model.predict(newData)
 
@@ -106,36 +95,24 @@ if __name__=='__main__':
     # Creating the graph
     fig = plt.figure(figsize=(10, 7))
     ax = fig.add_subplot(111, projection='3d')
+    ax.set_title('Multiple Linear Regression Best Fit Line (3D)')
+
     ax.set_xlabel('Town')
+
     ax.set_ylabel('Type', labelpad=40)
-    ax.set_zlabel('House Price')
-    ax.set_zlim(0,5000000)
-    ax.set_zticks(np.arange(0,5000000,500000))
     ax.set_yticks([1,2,3,4,5,6,7])
     ax.set_yticklabels(['Single Family', 'Two Family', 'Three Family', 'Four Family', 'Condo', 'Other', ''])
     ax.tick_params(axis='y', rotation=270)
 
+    ax.set_zlabel('House Price')
+    ax.set_zlim(0,5000000)
+    ax.set_zticks(np.arange(0,5000000,500000))
+
     # Plotting the training data in orange and prediction data in blue
-    #ax.scatter(X_test['MedInc'], X_test['AveBedrms'], y_pred, color='blue', label='Actual Data')
-    #ax.scatter(X_train['MedInc'], X_train['AveBedrms'], y_train, color='orange', label='Training Data')
-    ax.scatter(xAxis['Town'], xAxis['Type'], yAxis, color='red', label='Training Data')
+    ax.scatter(xAxis['Town'], xAxis['Type'], yAxis, color='orange', label='Training Data')
     ax.scatter(newData['Town'], newData['Type'], y_pred, color='blue')
-
     
-
-    # Getting the range of x values for the line of best fit
-    #x1_range = np.linspace(X_test['MedInc'].min(), X_test['MedInc'].max(), 100)
-    #x2_range = np.linspace(X_test['AveBedrms'].min(), X_test['AveBedrms'].max(), 100)
-    #x1, x2 = np.meshgrid(x1_range, x2_range)
-
-    # Creating a z axis for the line of best fit
-    #z = model.predict(np.c_[x1.ravel(), x2.ravel()]).reshape(x1.shape)
-
-    # Draw the line of best fit
-    #ax.plot_surface(x1, x2, z, color='orange', alpha=0.5, rstride=100, cstride=100)
-
-    
-    ax.set_title('Multiple Linear Regression Best Fit Line (3D)')
-    plt.savefig("output.jpg", dpi=300)
-    plt.show()
+    # Outputting the graph
+    #plt.savefig("resources/output.jpg", dpi=300)
+    #plt.show()
 
